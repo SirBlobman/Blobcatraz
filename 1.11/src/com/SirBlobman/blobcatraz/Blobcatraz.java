6e133c802f07f96e0d4625486bbd94dd79800da5
SirBlobman <blobmansrealm@gmail.com>
2017-01-07 20:54:59 -0500

    Version 0.2.0
    Started on the 2nd recode
    This will prevent memory leaks
    I also changed the versioning system

--- 1.11/src/com/SirBlobman/blobcatraz/Blobcatraz.java
+++ 1.11/src/com/SirBlobman/blobcatraz/Blobcatraz.java
@@ -1,112 +1,29 @@
 package com.SirBlobman.blobcatraz;
 
-import java.io.File;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.SirBlobman.blobcatraz.command.CommandAFK;
-import com.SirBlobman.blobcatraz.command.CommandBlobcatraz;
-import com.SirBlobman.blobcatraz.command.CommandFly;
-import com.SirBlobman.blobcatraz.command.CommandGamemode;
-import com.SirBlobman.blobcatraz.command.CommandHeal;
-import com.SirBlobman.blobcatraz.command.CommandMOTD;
-import com.SirBlobman.blobcatraz.command.CommandSpawner;
-import com.SirBlobman.blobcatraz.config.ConfigBlobcatraz;
-import com.SirBlobman.blobcatraz.config.ConfigLanguage;
-import com.SirBlobman.blobcatraz.listener.ListenAFK;
-import com.SirBlobman.blobcatraz.listener.ListenAntiTNT;
-import com.SirBlobman.blobcatraz.listener.ListenAutoPickup;
-import com.SirBlobman.blobcatraz.listener.ListenFreeze;
-import com.SirBlobman.blobcatraz.listener.ListenGiantLoot;
-import com.SirBlobman.blobcatraz.listener.ListenHubEffects;
-import com.SirBlobman.blobcatraz.listener.ListenLoginLogout;
-import com.SirBlobman.blobcatraz.listener.ListenMOTD;
-import com.SirBlobman.blobcatraz.listener.ListenSlime;
 import com.SirBlobman.blobcatraz.utility.Util;
 
 /**
- * Main class for the plugin "Blobcatraz"<br/>
- * Used by all the extensions to make sure they work<br/>
- * Copyright &copy; SirBlobman 2017
+ * Main class for Blobcatraz<br/>
+ * To create an addon, you must add "{@code depend: [Blobcatraz]}" to your {@code plugin.yml}<br/>
+ * You can also extend this class, but that is not required
  * @author SirBlobman
  * @see JavaPlugin
  */
 public class Blobcatraz extends JavaPlugin
 {
-	/**
-	 * Instance of Blobcatraz
-	 * Do not use before it is enabled
-	 */
-	public static Blobcatraz instance;
-	public static YamlConfiguration config;
-	public static File folder;
-	
 	@Override
 	public void onEnable()
 	{
-		instance = this;
-		folder = getDataFolder();
-		configs();
-		commands();
-		events();
-		Util.broadcast(Util.enable);
+		super.onEnable();
+		Util.broadcast("&2Enabling...");
 	}
 	
 	@Override
 	public void onDisable()
 	{
-		Util.broadcast(Util.disable);
-	}
-	
-	private void configs()
-	{
-		ConfigLanguage.load();
-		ConfigBlobcatraz.load();
-	}
-	
-	private void commands()
-	{
-		c("blobcatraz", new CommandBlobcatraz(), new CommandBlobcatraz());
-		
-		c("afk", new CommandAFK(), null);
-		c("fly", new CommandFly(), null);
-		c("feed", new CommandHeal(), null);
-		c("gamemode", new CommandGamemode(), null);
-		c("gma", new CommandGamemode(), null);
-		c("gmc", new CommandGamemode(), null);
-		c("gms", new CommandGamemode(), null);
-		c("gmsp", new CommandGamemode(), null);
-		c("heal", new CommandHeal(), null);
-		c("setmotd", new CommandMOTD(), null);
-		c("spawner", new CommandSpawner(), new CommandSpawner());
-	}
-	
-	private void events()
-	{
-		Util.regEvents
-		(
-			new ListenAFK(),
-			new ListenAntiTNT(),
-			new ListenAutoPickup(),
-			new ListenLoginLogout(),
-			new ListenFreeze(),
-			new ListenHubEffects(),
-			new ListenGiantLoot(),
-			new ListenSlime(),
-			new ListenMOTD()
-		);
-		Bukkit.getScheduler().runTaskTimer(this, new ListenHubEffects(), 0, 20);
-	}
-	
-	protected void c(String cmd, CommandExecutor ce, TabCompleter tc)
-	{
-		PluginCommand pc = getCommand(cmd);
-		if(ce != null) pc.setExecutor(ce);
-		if(tc != null) pc.setTabCompleter(tc);
+		super.onDisable();
+		Util.broadcast("&4Disabling...");
 	}
 }
--- 1.11/src/com/SirBlobman/blobcatraz/utility/Util.java
+++ 1.11/src/com/SirBlobman/blobcatraz/utility/Util.java
@@ -1,88 +1,53 @@
 package com.SirBlobman.blobcatraz.utility;
 
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
-import java.util.TreeMap;
+import java.io.PrintStream;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Server;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.EntityType;
-import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 
-import com.SirBlobman.blobcatraz.Blobcatraz;
-import com.SirBlobman.blobcatraz.config.ConfigBlobcatraz;
-import com.SirBlobman.blobcatraz.config.ConfigLanguage;
-import com.SirBlobman.blobcatraz.config.ConfigSymbols;
-import com.google.common.collect.Maps;
-
 /**
- * Utility class for Blobcatraz<br/>
- * Contains many useful utilities that prevent code duplication
+ * Default Utility class for Blobcatraz
+ * Has a lot of useful stuff
  * @author SirBlobman
- *
  */
 public class Util
 {
-	protected static Blobcatraz plugin = Blobcatraz.instance;
-	protected static final Server S = Bukkit.getServer();
-	protected static final PluginManager PM = S.getPluginManager();
-	protected static final Random R = new Random();
-
-	protected static YamlConfiguration CB = ConfigBlobcatraz.load();
-	protected static YamlConfiguration CL = ConfigLanguage.load();
+	private static final Server SERVER = Bukkit.getServer();
+	private static final PluginManager PLUGIN_MANAGER = SERVER.getPluginManager();
 	
-	public static final String prefix = option("prefix");
-	public static final String prefix2 = uncolor(prefix);
-	public static final String enable = option("enable");
-	public static final String disable = option("disable");
-	
-	public static final String IA = prefix + option("command.invalid arguments");
-	public static final String NEA = prefix + option("command.not enough arguments");
-	public static final String TMA = prefix + option("command.too many arguments");
-	public static final String notPlayer = prefix + option("error.not player");
-	public static final String notLiving = prefix + option("error.not living");
-	
-	public static final ItemStack AIR = new ItemStack(Material.AIR);
+	public static final String PREFIX = color("&3{&bBlobcatraz&3} &f");
+	public static final String PREFIX2 = uncolor(PREFIX);
 	
 	/**
-	 * Get a configured message
-	 * @param key Path to the message
-	 * @param format Formatting Objects
-	 * @return Formatted message
+	 * Print a message to the console<br/>
+	 * Colors will be removed
+	 * @param msg Message to print
 	 */
-	public static String option(String key, Object... format)
+	public static void print(String msg)
 	{
-		CL = ConfigLanguage.load();
-		String o = CL.getString(key);
-		if(o == null) o = key;
-		String f = format(o, format);
-		return f;
-	}
-	
-	public static String format(String o, Object... format)
-	{
-		String c = color(o);
-		String s = symbolize(c);
-		String f = String.format(s, format);
-		return f;
+		String prt = uncolor(PREFIX2 + msg);
+		PrintStream ps = System.out;
+		ps.println(prt);
 	}
 	
 	/**
-	 * Add some color to a message
-	 * @param o String to add color
-	 * @return The colored message<br/><b>&amp;</b> will be replaced with <b>&sect;</b>
+	 * Broadcast a message<br/>
+	 * The message will be seen by entities, players, and the console
+	 * @param msg Message to broadcast
+	 */
+	public static void broadcast(String msg)
+	{
+		String bc = color(PREFIX + msg);
+		SERVER.broadcastMessage(bc);
+	}
+	
+	/**
+	 * Add some chat colors to your message<br/>
+	 * <b>&amp;</b> will be replaced with <b>&sect;</b>
+	 * @param o original message
+	 * @return <span style="color: pink">colored message</span>
 	 */
 	public static String color(String o)
 	{
@@ -90,146 +55,15 @@ public class Util
 		return c;
 	}
 	
+	/**
+	 * Remove all chat colors and formatting<br/>
+	 * <b>&sect;</b> will be removed, along with the letter/number after it<br/>
+	 * @param c <span style="color: pink">colored message</span>
+	 * @return un-formatted message
+	 */
 	public static String uncolor(String c)
 	{
 		String o = ChatColor.stripColor(c);
 		return o;
-	}
-	
-	public static String symbolize(String o)
-	{
-		Map<String, Character> map = ConfigSymbols.symbols();
-		for(Entry<String, Character> e : map.entrySet())
-		{
-			String s = e.getKey();
-			char c = e.getValue();
-			String c2 = ConfigSymbols.CtoS(c);
-			o = o.replace(s, c2);
-		}
-		return o;
-	}
-	
-	/**
-	 * Show a message in console
-	 * @param msg Message to show
-	 * @see String
-	 * @see java.io.PrintStream#println(String)
-	 */
-	public static void print(String msg)
-	{
-		String l = prefix2 + msg;
-		System.out.println(l);
-	}
-	
-	public static void broadcast(String msg)
-	{
-		String c = prefix + format(msg);
-		S.broadcastMessage(c);
-	}
-	
-	public static void regEvents(Listener... ls)
-	{
-		for(Listener l : ls) PM.registerEvents(l, plugin);
-	}
-	
-	public static String getFinal(String[] args, int s)
-	{
-		StringBuilder sb = new StringBuilder();
-		for(int i = s; i < args.length; i++)
-		{
-			if(i != s) sb.append(" ");
-			sb.append(args[i]);
-		}
-		return sb.toString();
-	}
-	
-	public static List<String> getMatching(List<String> o, String arg)
-	{
-		if(o == null || arg == null) return null;
-		List<String> match = new ArrayList<String>();
-		for(String s : o)
-		{
-			String l = s.toLowerCase();
-			String a = arg.toLowerCase();
-			if(l.startsWith(a)) match.add(s); 
-		}
-		return match;
-	}
-	
-	public static String json(String o)
-	{
-		String txt = "{\"text\": \"" + o + "\"}";
-		return txt;
-	}
-	
-	private static final TreeMap<Integer, String> romans()
-	{
-		TreeMap<Integer, String> map = Maps.newTreeMap();
-		map.put(1000, "M");
-		map.put(900, "CM");
-		map.put(500, "D");
-		map.put(400, "CD");
-		map.put(100, "C");
-		map.put(90, "XC");
-		map.put(50, "L");
-		map.put(40, "XL");
-		map.put(10, "X");
-		map.put(9, "IX");
-		map.put(5, "V");
-		map.put(4, "IV");
-		map.put(1, "I");
-		return map;
-	}
-	
-	public static String numberToRoman(int number)
-	{
-		TreeMap<Integer, String> roman = romans();
-		int n = roman.floorKey(number);
-		if(number == n) return roman.get(number);
-		return roman.get(n) + numberToRoman(number - 1);
-	}
-	
-	public static String monetize(double d)
-	{
-		NumberFormat nf = NumberFormat.getCurrencyInstance();
-		String c = nf.format(d);
-		return c;
-	}
-	
-	public static List<Block> blocks(Location l1, Location l2)
-	{
-		List<Block> blocks = new ArrayList<Block>();
-		int topX = (l1.getBlockX() < l2.getBlockX() ? l2.getBlockX() : l1.getBlockX());
-		int botX = (l1.getBlockX() > l2.getBlockX() ? l2.getBlockX() : l1.getBlockX());
-		int topY = (l1.getBlockY() < l2.getBlockY() ? l2.getBlockY() : l1.getBlockY());
-        int botY = (l1.getBlockY() > l2.getBlockY() ? l2.getBlockY() : l1.getBlockY());
-        int topZ = (l1.getBlockZ() < l2.getBlockZ() ? l2.getBlockZ() : l1.getBlockZ());
-        int botZ = (l1.getBlockZ() > l2.getBlockZ() ? l2.getBlockZ() : l1.getBlockZ());
-        for(int x = botX; x <= topX; x++)
-        {
-        	for(int z = botZ; z <= topZ; z++)
-        	{
-        		for(int y = botY; y <= topY; y++)
-        		{
-        			World w = l1.getWorld();
-        			Block b = w.getBlockAt(x, y, z);
-        			blocks.add(b);
-        		}
-        	}
-        }
-        return blocks;
-	}
-	
-	public static List<String> living()
-	{
-		List<String> list = new ArrayList<>();
-		EntityType[] types = EntityType.values();
-		for(EntityType et : types)
-		{
-			String type = et.name();
-			if(et.isAlive()) list.add(type);
-		}
-		list.remove("PLAYER");
-		return list;
 	}
 }
